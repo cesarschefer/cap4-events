6718d972fbe2740df5698e6110e87952f17703a7
cesarschefer <cesarschefer@gmail.com>
2025-04-05 14:00:50 -0300

    add comment [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 
+// comento algo para testear skip workflow
+
 function App() {
   return (
     <>
